f57383b3c44d813d9a2cc1f6ef8790d7d60b5518
Carlos <carlos@pop-os.localdomain>
2023-10-24 11:49:41 +0200

    [ADD] Primer commit

---
+++ app/Http/Controllers/ImagenController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Intervention\Image\Facades\Image;
+
+class ImagenController extends Controller
+{
+    public function store(Request $request){
+        $image = $request->file('file');
+
+        $nombreImagen = Str::uuid() . "." . $image->extension();
+
+        $imagenServidor = Image::make($image);
+        $imagenServidor->fit(1000,1000);
+
+        $imagenPath = public_path('uploads') . '/' . $nombreImagen;
+        $imagenServidor->save($imagenPath);
+        
+        return response()->json(['imagen' => $nombreImagen]);
+    }
+}
---
+++ resources/views/principal.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section("titulo")
+    wenosdias
+@endsection
+
+@section("contenido")
+    <h2>conetnido variadete</h2>
+@endsection
