87e55088362cc2e37a53a9e9009591ade06128ed
carlosmoncho <carlosmonchomolla@gmail.com>
2023-10-26 11:21:02 +0200

    [ADD] Editar perfil v2

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Intervention\Image\Facades\Image;
 
 class PerfilController extends Controller
@@ -21,7 +22,8 @@ class PerfilController extends Controller
         $request->request->add(['username' => Str::slug($request->username)]);
 
         $this->validate($request,[
-            'username' => ['required', 'unique:users,username,'.auth()->user()->id,'min:3','max:20','not_in:editar-perfil']
+            'username' => ['required', 'unique:users,username,'.auth()->user()->id,'min:3','max:20','not_in:editar-perfil'],
+            'email' => ['required','unique:users,username,'.auth()->user()->email,'email','max:60']
         ]);
 
         if($request->imagen){
@@ -36,9 +38,27 @@ class PerfilController extends Controller
             $imagenServidor->save($imagenPath);
         }
 
+
         $usuario = User::find(auth()->user()->id);
+
+        if(Hash::check($request->password, auth()->user()->password) ){
+            if(!is_null($request->newpassword) && $request->newpassword != $request->password){
+                $usuario->password = $request->newpassword;        
+            }elseif($request->newpassword === $request->password ){
+                return back()->with('newpasswordMensaje', 'El password nuevo es igual al antiguo');
+            }else{
+                return back()->with('newpasswordMensaje', 'El password esta vacio');
+            }
+        }else if(!is_null($request->password)){
+            return back()->with('passwordMensaje', 'El password no es correcto');
+        }else if(!is_null($request->newpassword) && is_null($request->password)){
+            return back()->with('passwordMensaje', 'Debes de poner el password actual para poder cambiarlo');
+        }
+
+
         $usuario->username = $request->username;
         $usuario->imagen = $nombreImagen ?? auth()->user()->imagen ?? NULL;
+        $usuario->email = $request->email;
         $usuario->save();
 
         return redirect()->route('posts.index',$usuario->username);
--- resources/views/perfil/index.blade.php
+++ resources/views/perfil/index.blade.php
@@ -39,7 +39,55 @@
                     value=""
                     accept=".jpg, .jpeg, .png">
                 </div>
+                <div class="mb-5">
+                    <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Email
+                    </label>
+                    <input 
+                    id="email" 
+                    name="email" 
+                    type="text" 
+                    placeholder="Tu Email de registro" 
+                    class="border p-3 w-full rounded-lg @error('email') border-red-500 @enderror"
+                    value="{{auth()->user()->email}}">
+                    @error('email')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
+                            {{$message}}
+                        </p>
+                    @enderror
+                </div>
 
+                <div class="mb-5">
+                    <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Cambiar Password
+                    </label>
+                    <input 
+                    id="password" 
+                    name="password" 
+                    type="password" 
+                    placeholder="Password actual" 
+                    class="border p-3 w-full rounded-lg @error('password') border-red-500 @enderror">
+                    @if (session('passwordMensaje'))
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
+                            {{session('passwordMensaje')}}
+                        </p>
+                    @endif
+                </div>
+                <div class="mb-5">
+                    <label for="newpassword" class="mb-2 block uppercase text-gray-500 font-bold">
+                    </label>
+                    <input 
+                    id="newpassword" 
+                    name="newpassword" 
+                    type="password" 
+                    placeholder="Password nueva" 
+                    class="border p-3 w-full rounded-lg">
+                    @if(session('newpasswordMensaje'))
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
+                            {{session('newpasswordMensaje')}}
+                        </p>
+                    @endif
+                </div>
                 <input type="submit" value="Guardar Cambios" class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
 
             </form>
